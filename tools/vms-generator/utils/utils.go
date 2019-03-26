0baeb4098e6335cbe970893a3fc15766f9b9c55a
Denis Ollier <dollierp@redhat.com>
2019-03-26 18:39:13 +0100

    tests: pull containerDisk images from public docker registry
    Signed-off-by: Denis Ollier <dollierp@redhat.com>

--- tools/vms-generator/utils/utils.go
+++ tools/vms-generator/utils/utils.go
@@ -88,8 +88,8 @@ const windowsFirmware = "5d307ca9-b3ef-428c-8861-06e72d69f223"
 
 const apiVersion = "kubevirt.io/v1alpha3"
 
-var DockerPrefix = "registry:5000/kubevirt"
-var DockerTag = "devel"
+var DockerPrefix = "docker.io/kubevirt"
+var DockerTag = "latest"
 
 var gracePeriod = int64(0)
 
